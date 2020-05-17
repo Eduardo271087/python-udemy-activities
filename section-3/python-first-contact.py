e845193c8bd256a0fcc2eb1aa75a6bed426fbfee
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-17 16:33:51 -0500

    Add section-3. Python first contact

---
+++ section-3/python-first-contact.py
@@ -0,0 +1,35 @@
+print("5 = " + str(5))
+
+print("12.5 = " + str(12.5))
+
+print("5 + 4 = " + str(5 +4))
+
+print("3 * 5 = " + str(3 * 5))
+
+print("3 - 2 = " + str(3 - 2))
+
+print("2 - 3 = " + str(2 - 3))
+
+print("5 / 8 = " + str(5 / 8))
+
+print("9 % 2 = " + str(9 % 2))
+
+print("8 ** 6 = " + str(8 ** 6))
+
+print("8 - 9 + 4 * 8 - 4 = " + str(8 - 9 + 4 * 8 - 4))
+
+variable = 5
+print("variable = " + str(variable))
+print("variable + 5 = " + str(variable + 5))
+
+alvaro = "alvaro"
+variable = alvaro
+print("variable = " + variable)
+
+variable = "chirou"
+print("variable = " + variable)
+
+variable = 10
+print("variable = " + str(variable))
+
+print("variable = " + str(variable * variable))
