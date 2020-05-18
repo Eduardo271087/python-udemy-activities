558215117b69720922cbd828317a085d22a88910
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-18 16:49:42 -0500

    Add section-3 - Strings (part 1)

---
+++ section-3/strings-part-1.py
@@ -0,0 +1,15 @@
+print('Alvaro Chirou')
+
+stre = "Hola estudiantes"
+print(type(stre))
+
+print('Alvaro saluda a sus "Estudiantes"')
+
+print("Hola 'Alvaro'")
+
+print("Este texto tiene\nuna nueva línea")
+
+print('C:\nombres\clases')
+
+# La r ignora los caracteres de escape
+print(r'C:\nombres\clases')
