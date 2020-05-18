1f686681a857f3af6bb18d56c40247ddf3662b37
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-18 16:56:32 -0500

    Add section-3 - Strings (part 2)

---
+++ section-3/strings-part-2.py
@@ -0,0 +1,14 @@
+print("""De esta manera
+nos ahorramos a nuna
+pero no a \tuna, que es para la tabulación""")
+
+variable = "Teoría sobre este tema donde salto de línea es \ncon dos líneas"
+print(variable)
+
+print(variable + variable)
+
+print(variable)
+
+primero = "primera parte"
+segundo = " segunda parte"
+print("fusion de " + primero + segundo)
