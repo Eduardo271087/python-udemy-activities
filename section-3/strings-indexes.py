aef5bf819d314b425793d98f20d4b2bac50749ae
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-18 17:05:51 -0500

    Add section-3 - Strings indexes

---
+++ section-3/strings-indexes.py
@@ -0,0 +1,25 @@
+profesor = "Álvaro Chirou"
+
+print("profesor[2]: '" + profesor[2] + "'")
+
+print("profesor[7]: '" + profesor[7] + "'")
+
+print("profesor[6]: '" + profesor[6] + "'")
+
+print("profesor[-1]: '" + profesor[-1] + "'")
+
+print("profesor[-2]: '" + profesor[-2] + "'")
+
+print("profesor[2:5]: '" + profesor[2:5] + "'")
+
+print("profesor[:5]: '" + profesor[:5] + "'")
+
+print("profesor[2:]: '" + profesor[2:] + "'")
+
+print("profesor[:]: '" + profesor[:] + "'")
+
+print("profesor: '" + profesor + "'")
+
+print("profesor[2:] + profesor[:2]: '" + profesor[2:] + profesor[:2] + "'")
+
+print("len(profesor): '" + str(len(profesor)) + "'")
