156df264ba9742a34b6ea1a71875def4df590255
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-18 17:27:10 -0500

    Add section-3 - Keyboard inputs

---
+++ section-3/keyboard-inputs.py
@@ -0,0 +1,16 @@
+print(input())
+
+dato = input()
+print("Dato: '" + dato + "'")
+
+dato = input()
+print("Dato int: '" + str(int(dato) + 55) + "'")
+
+dato = input()
+print("Dato float: '" + dato + "'")
+
+dato = input()
+print("Dato int: '" + str(float(dato) + 55) + "'")
+
+dato = float(input("Introduce un valor decimal o entero: "))
+print("Último dato: '" + str(dato) + "'")
