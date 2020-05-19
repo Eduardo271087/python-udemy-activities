e140c4cf786236a254ab657c4da6aebd6d3fba35
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-19 12:27:02 -0500

    Add section-4 - Relational operators

---
+++ section-4/relational-operators.py
@@ -0,0 +1,60 @@
+print(3 == 3)
+
+print(3 == 2)
+
+print(1 != 2)
+
+print(1 != 1)
+
+print(3 > 4)
+
+print(5 > 4)
+
+print(4 > 4)
+
+print(5 < 4)
+
+print(4 >= 4)
+
+print(4 <= 4)
+
+print(4 <= 5)
+
+Alvaro = 20
+Chirou = 5
+Estudiantes = 0
+
+print(Alvaro > Chirou)
+
+print(Alvaro != Chirou)
+
+print(Alvaro == Chirou * 4)
+
+print("Alvaro" == "Alvaro")
+
+print("Alvaro" == "Álvaro")
+
+print("Alvaro" != "Álvaro")
+
+texto = "Alvaro"
+
+print(texto[0] == "A")
+
+print(texto[0] == "a")
+
+lista1 = [3, 4, 5]
+lista2 = [6, 7, 8]
+
+print(lista1 == lista2)
+
+print(len(lista1) == len(lista2))
+
+print(True == 1)
+
+print(False == 0)
+
+print(True * 5)
+
+print(False * 5)
+
+print(True * False)
