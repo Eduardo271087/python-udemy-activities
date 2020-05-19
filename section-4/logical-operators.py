60d7fa3e794c6e987430a3212997cf81be1ffd18
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-19 11:57:43 -0500

    Add section-4 - Logical operators

---
+++ section-4/logical-operators.py
@@ -0,0 +1,39 @@
+print(2 + 4 == 7)
+
+print(2 + 4 == 6)
+
+print(not True)
+
+print(not False == True)
+
+print(False and False)
+
+print(True and True)
+
+numero = 10
+print(numero > 5 and numero < 15)
+
+print(numero > 5 and numero > 15)
+
+print(True and False)
+
+print(numero > 5 or numero > 15)
+
+print(True or True)
+
+print(True or False)
+
+print(False or False)
+
+palabra = 'Álvaro'
+print(palabra == "Salir" or palabra == "Terminar" or palabra == "Exit")
+
+print(palabra == "Salir" or palabra == "Álvaro" or palabra == "Exit")
+
+print(palabra[0] == "l")
+
+print(palabra[0] == "A")
+
+print(palabra[0] == "Á")
+
+print(palabra[0] == "Á" or palabra[1] == "l")
