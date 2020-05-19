a8df7bf80a11bb964ae8601dfdf1b4a13dcd215c
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-19 12:34:58 -0500

    Add section-4 - Nested expressions and assignment operators

---
+++ section-4/nested-expressions-assignment-operators.py
@@ -0,0 +1,22 @@
+primero = 8
+segundo = 10
+print(primero * segundo - 4 ** primero <= 8 or not (primero % segundo) == 0)
+
+valor = 10
+valor += 10
+print(valor)
+
+valor -= 10
+print(valor)
+
+valor *= 10
+print(valor)
+
+valor /= 10
+print(valor)
+
+valor %= 10
+print(valor)
+
+valor **= 10
+print(valor)
