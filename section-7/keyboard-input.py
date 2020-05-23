8f3d523094c4652c9538525044ae61e1a7ade3a9
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-23 15:11:11 -0500

    Add section-7 - Keyboard input

---
+++ section-7/keyboard-input.py
@@ -0,0 +1,11 @@
+ingreso = input("Ingresa tu clase: ")
+
+print(ingreso)
+
+lista = []
+print("Ingresa ya mismo 5 números")
+
+for i in range(5):
+  lista.append(input("Ingresa un número: "))
+
+print(lista)
