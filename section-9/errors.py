214ac5c9db919bba5c26955017cd54aeef6589df
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-24 11:18:43 -0500

    Add section-9 - Errors

---
+++ section-9/errors.py
@@ -0,0 +1,16 @@
+#   File "/home/eduardo-fernandez/source/python-udemy-activities/section-9/errors.py", line 1
+#     print('alvaro
+#               ^
+# SyntaxError: EOL while scanning string literal
+# print('alvaro
+
+#   File "/home/eduardo-fernandez/source/python-udemy-activities/section-9/errors.py", line 8, in <module>
+#     ping('cualquiera')
+# NameError: name 'ping' is not defined
+# ping('cualquiera')
+
+lista = []
+#   File "/home/eduardo-fernandez/source/python-udemy-activities/section-9/errors.py", line 13, in <module>
+#     lista.pop()
+# IndexError: pop from empty list
+# lista.pop()
