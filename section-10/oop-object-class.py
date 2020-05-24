7dc5afb64af68cbbeed00f98ef48643740a51396
Eduardo Fernández <eduardo.fernandez@smartmatic.com>
2020-05-24 11:54:47 -0500

    Add section-10 - OOP objects and classes

---
+++ section-10/oop-object-class.py
@@ -0,0 +1,14 @@
+# Definición de clase en Python
+class Estudiante:
+  pass
+
+# Instancia de una clase en Python
+estudiante = Estudiante()
+
+print(type(estudiante))
+
+def EstudianteFuncion():
+  pass
+
+print(type(EstudianteFuncion))
+
